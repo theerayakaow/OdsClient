47ffc3b0762d1aaa4bc984cd0e1ec81542c5f4a9
theerayakaow <kaowtheeraya@hotmail.com>
2025-04-21 06:02:58 +0700

    chnage name 2

--- android/app/src/main/java/com/clientmobileapp/MainActivity.kt
+++ android/app/src/main/java/com/clientmobileapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.clientmobileapp
+package com.ondemandsalaryclient
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ClientMobileApp"
+  override fun getMainComponentName(): String = "OnDemandSalaryClient"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/clientmobileapp/MainApplication.kt
+++ android/app/src/main/java/com/clientmobileapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.clientmobileapp
+package com.ondemandsalaryclient
 
 import android.app.Application
 import com.facebook.react.PackageList
